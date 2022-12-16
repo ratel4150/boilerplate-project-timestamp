918fbb0ec966fdf5a6cb104ccfae79eb6d33fa49
ratel4150 <rratel76@gmail.com>
2022-12-16 20:21:02 +0100

    ok

--- index.js
+++ index.js
@@ -20,13 +20,52 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+   
+  let dateString=req.params.date
+  
+  
+   if (/[0-9]{4}-[0-9]{2}-[0-9]{2}/.test(dateString)) {
+    
+    let dateUnix=Date.parse(dateString)
+    console.log(dateUnix)
+    res.json({ unix: dateUnix, utc: new Date(dateString).toUTCString() });
+
+
+  } else {
+    
+
+    let dataObject =new Date(Number(dateString))
+    console.log(dataObject);
+    let unixToUTC=dataObject.toUTCString() 
+  
+
+    res.json({ unix: dateString, utc:unixToUTC});
+    
+  } 
+ /*  A 4 digit number is a valid ISO-8601 for the beginning of that year
+  5 digits or more must be a unix time, until we reach a year 10,000 problem
+  let dateString=req.params.date_string
+  console.log(dateString);
+  if (/\d{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString);
+    Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  } */
+  
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
